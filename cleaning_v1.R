62b5fb2c0160e0d0ad2f19540697e9d775637d14
Luis Andrade <landrad78@gmail.com>
2017-11-05 17:18:05 -0700

    quiz1

--- cleaning_v1.R
+++ cleaning_v1.R
@@ -14,26 +14,26 @@ stats = NULL
 for (i in 1:length(myFiles)) {
         tic = proc.time()
         # Read a chunck of data
-        print(paste("Reading file:", myFiles[i], "...\n"))
+        print(paste("Reading file:", myFiles[i], "..."))
         con = file(paste0(data.folder, myFiles[i]), "r")
         myLines = readLines(con)
         close(con)
-        print("Finished reading file.\n")
+        print("Finished reading file.")
         
         # Start up a parallel cluster
         cl = makeCluster(detectCores())
         
         # clean text
-        print(paste("Cleaning text in file:", myFiles[i], "...\n"))
+        print(paste("Cleaning text in file:", myFiles[i], "..."))
         myText = parLapply(cl, myLines, cleanLine)
-        print("Finished cleaning text.\n")
+        print("Finished cleaning text.")
         
-        print(paste("Calculating descriptives in file:", myFiles[i], "...\n"))
+        print(paste("Calculating descriptives in file:", myFiles[i], "..."))
         fileStats = parLapply(cl, myText, describeLines)
-        print("Finished calculating stats.\n")
+        print("Finished calculating stats.")
         
         # save objects to hard drive
-        print("Saving objects...\n")
+        print("Saving objects...")
         objName = strsplit(myFiles[i], split = "\\.")[[1]][2]
         saveRDS(myText, paste0(data.folder, objName, ".rds"))
         saveRDS(fileStats, paste0(data.folder, objName, "_stats.rds"))
@@ -44,8 +44,10 @@ for (i in 1:length(myFiles)) {
         # compute descriptive statistics
         stats = rbind(stats, c(objName, textStats(fileStats)))
         totTime = proc.time() - tic
-        print(paste("Total processing time:", totTime[3] %>% round(2), "\n"))
+        print(paste("Total processing time:", totTime[3] %>% round(2)))
 }
 
 stats
 write.csv(stats, paste0(data.folder, "textStatistics.csv"), row.names = F)
+
+
